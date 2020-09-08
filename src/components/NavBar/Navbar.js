0ee065aae747960a34f9062e56a3550ef169b045
Karl Cui <karl.cui@mail.utoronto.ca>
2020-09-08 00:51:49 -0700

    Create pages and components

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,7 @@
+class NavBar extends React.Component {
+    render() {
+      return (
+        <p>hello world</p>
+      );
+    }
+  }
---
+++ src/components/Pages/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UnderConstruction from './UnderConstruction/UnderConstruction';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
