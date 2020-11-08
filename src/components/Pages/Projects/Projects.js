142e7eb5ff5f4870602338717c4b2fc9ed47a775
Karl Cui <karl.cui@mail.utoronto.ca>
2020-11-08 00:09:22 -0800

    Refactor project page for rendering list of projects

--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -13,8 +13,98 @@ import CreviceIcon from '../../../assets/project-icons/crevice.jpg';
 
 
 class Projects extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        // all current projects
+        this.current_projects = [
+            {
+                title: "Piazza Bot",
+                description: [
+                    "Can we create a bot to automatically intervene when a student makes a post that's a duplicate of or similar to a question that has been answered already?"
+                ],
+                img: PlaceHolder,
+            }
+        ]
+
+        // all past projects
+        this.past_projects = [
+            {
+                title: "2D to 3D Reconstruction",
+                url: "https://github.com/Karl-Cui/2dto3dreconstruction",
+                description: [
+                    "We had to do a project for a course, but what we ended up making is much more than just a course project.", 
+                    "We created 3D CAD models from 2D images using a mix of machine learning with PyTorch, 3D point cloud registration, and meshing algorithms."
+                ],
+                img: ReconstructionIcon,
+            },
+            {
+                title: "Ease",
+                url: "https://github.com/Karl-Cui/ai-eat-green",
+                description: [
+                    "Android and iOS app that uses a recommendation system on Azure to promote healthy eating!"
+                ],
+                img: EaseIcon,
+            },
+            {
+                title: "Neurostack",
+                url: "https://neurotechuoft.com/neurostack",
+                description: [
+                    "Streaming brain waves to machine learning services, made easy.",
+                    "In my time as team lead, we created an open-source EEG signal-processing API and research and deployed several different microservices onto Google Cloud."
+                ],
+                img: NeurostackIcon,
+            },
+            {
+                title: "Showtime",
+                url: "https://github.com/Karl-Cui/show-time",
+                description: [
+                    "Web app for rating and commenting on TV shows and making watchlists using the MEAN (MongoDB, Express, Angular, NodeJS) stack!"
+                ],
+                img: ShowtimeIcon,
+            },
+            {
+                title: "MindType",
+                url: "https://github.com/neurotechuoft/MindType",
+                description: [
+                    '"How cool would it be to type without a keyboard?" "Pretty cool."',
+                    "So we made a mind-controlled keyboard. To determine which key the user wants to type, we flash keys on a keyboard and see which ones correspond to a P300 signal."
+                ],
+                img: MindTypeIcon,
+            },
+            {
+                title: "Crevice",
+                url: "https://docs.google.com/document/d/1Rtq9fXhKK6gDF01ya7LV6snOQ9hyHz0_DZLcvBMUq2o/edit?usp=sharing",
+                description: [
+                    "Which design patterns are effective in educational video games?",
+                    "To try to answer this, I helped professor Steve Engels design an educational video with Unity that aims to teach high school students the intuition behind a branch of physics, optics."
+                ],
+                img: CreviceIcon,
+            }
+        ]
+    }
     
     render() {
+        const listParagraphs = (paras) => 
+            paras.map((para) => 
+                <p>{para}</p>
+            );
+
+        // display each project
+        const listProjects = (projects) => 
+            projects.map((project) => 
+                <a href={project.url} target="_blank" rel="noopener noreferrer">
+                    <div class="project">
+                        <img src={project.img} alt={project.title + " icon"}/>
+                        <div>
+                            <h2>{project.title}</h2>
+                            {listParagraphs(project.description)}
+                        </div>
+                    </div>
+                </a>
+            );
+
         return (
             <div>
                 <NavBar/>
@@ -27,18 +117,7 @@ class Projects extends React.Component {
                     <h1 class="projects-section-title">In progress</h1>
     
                     <div class="projects-grid">
-    
-                        {/* Pizza bot */}
-                        <div class="project">
-                            <img src={PlaceHolder} alt="Placeholder icon"/>
-    
-                            <div>
-                                <h2>Piazza bot</h2>
-                                {/* <h3>NLP, Selenium, Python</h3> */}
-                                <p>Can we create a bot to automatically intervene when a student makes a post that's a duplicate of or similar to a question that has been answered already?</p>
-                            </div>
-                        </div>
-    
+                        {listProjects(this.current_projects)}
                     </div>
                 </div>
     
@@ -46,90 +125,7 @@ class Projects extends React.Component {
                     <h1 class="projects-section-title">Past Projects</h1>
     
                     <div class="projects-grid">
-    
-                        {/* CSC420 2D to 3D reconstruction */}
-                        <a href="https://github.com/Karl-Cui/2dto3dreconstruction" target="_blank" rel="noopener noreferrer">
-                            <div class="project">
-                                <img src={ReconstructionIcon} alt="2D to 3D reconstruction icon"/>
-    
-                                <div>
-                                    <h2>2D to 3D Reconstruction</h2>
-                                    {/* <h3>PyTorch, CAD models, OpenCV, Python</h3> */}
-                                    <p>We had to do a project for a course, but what we ended up making is much more than just a course project.</p>
-                                    <p>We created 3D CAD models from 2D images using a mix of machine learning with PyTorch, 3D point cloud registration, and meshing algorithms.</p>
-                                </div>
-                            </div>
-                        </a>
-    
-                        {/* AI Eat Green / Ease*/}
-                        <a href="https://github.com/Karl-Cui/ai-eat-green" target="_blank" rel="noopener noreferrer">
-                            <div class="project">
-                                <img src={EaseIcon} alt="Ease icon"/>
-    
-                                <div>
-                                    <h2>Ease</h2>
-                                    {/* <h3>Microsoft Azure, Angular, Ionic, TypeScript</h3> */}
-                                    <p>Android and iOS app that uses a recommendation system on Azure to promote healthy eating!</p>
-                                </div>
-                            </div>
-                        </a>
-    
-                        {/* Neurostack */}
-                        <a href="https://neurotechuoft.com/neurostack" target="_blank" rel="noopener noreferrer">
-                            <div class="project">
-                                <img src={NeurostackIcon} alt="Neurostack icon"/>
-    
-                                <div>
-                                    <h2>Neurostack</h2>
-                                    {/* <h3>Google Cloud Platform, signal processing, SocketIO, Flask, Python</h3> */}
-                                    <p>Streaming brain waves to machine learning services, made easy.</p>
-                                    <p>In my time as team lead, we created an open-source EEG signal-processing API and research and deployed several different microservices onto Google Cloud.</p>
-                                </div>
-                            </div>
-                        </a>
-    
-                        {/* Showtime */}
-                        <a href="https://github.com/Karl-Cui/show-time" target="_blank" rel="noopener noreferrer">
-                            <div class="project">
-                                <img src={ShowtimeIcon} alt="Showtime icon"/>
-    
-                                <div>
-                                    <h2>Showtime</h2>
-                                    
-                                    {/* <h3>MongoDB, Express, Angular, NodeJS, TypeScript</h3> */}
-                                    <p>Web app for rating and commenting on TV shows and making watchlists using the MEAN (MongoDB, Express, Angular, NodeJS) stack!</p>
-                                </div>
-                            </div>
-                        </a>
-    
-                        {/* MindType */}
-                        <a href="https://github.com/neurotechuoft/MindType" target="_blank" rel="noopener noreferrer">
-                            <div class="project">
-                                <img src={MindTypeIcon} alt="MindType icon"/>
-    
-                                <div>
-                                    <h2>MindType</h2>
-                                    {/* <h3>Python, Cython</h3> */}
-                                    <p>"How cool would it be to type without a keyboard?" "Pretty cool."</p>
-                                    <p>So we made a mind-controlled keyboard. To determine which key the user wants to type, we flash keys on a keyboard and see which ones correspond to a P300 signal.</p>
-                                </div>
-                            </div>
-                        </a>
-    
-                        {/* Crevice */}
-                        <a href="https://docs.google.com/document/d/1Rtq9fXhKK6gDF01ya7LV6snOQ9hyHz0_DZLcvBMUq2o/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
-                            <div class="project">
-                                <img src={CreviceIcon} alt="Crevice icon"/>
-    
-                                <div>
-                                    <h2>Crevice</h2>
-                                    {/* <h3>C#, Unity</h3> */}
-                                    <p>Which design patterns are effective in educational video games?</p>
-                                    <p>To try to answer this, I helped professor Steve Engels design an educational video with Unity that aims to teach high school students the intuition behind a branch of physics, optics.</p>
-                                </div>
-                            </div>
-                        </a>
-    
+                        {listProjects(this.past_projects)}
                     </div>
                 </div>
             </div>
