bfb99f3d532a6714d26c5553f9e77c3617a1dc04
Karl Cui <karl.cui@mail.utoronto.ca>
2020-11-21 23:56:14 -0800

    template for putnam problem

--- src/components/Pages/Blog/posts/putnam.js
+++ src/components/Pages/Blog/posts/putnam.js
@@ -5,7 +5,7 @@ const putnam =  {
     date: "Nov 18, 2020",
     content: `
 
-I was chatting with a friend when he posed an interesting problem from Putnam practice (as you do, naturally):
+I was chatting with a friend when he posed an interesting Putnam practice problem (as you do, naturally):
 
 `
 };
