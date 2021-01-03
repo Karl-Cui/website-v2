8ffe62eb284de3e25fc56ae5dcee0db5b6713ed6
Karl Cui <karl.cui@mail.utoronto.ca>
2021-01-02 23:58:45 -0800

    Create foobar_pt2.js

---
+++ src/components/Pages/Blog/posts/foobar_pt2.js
@@ -0,0 +1,12 @@
+const foobar = {
+    title: "Google Foobar Round 2",
+    title_short: "Foobar Round 2",
+    id: "foobar2",
+    date: "Jan 2, 2020",
+    content: `
+    
+So after finishing all the challenges in Google Foobar, I tried requesting another challenge (as you do). Interestingly enough, I received another problem, and a very difficult one at that.
+    
+};
+
+export default foobar;
